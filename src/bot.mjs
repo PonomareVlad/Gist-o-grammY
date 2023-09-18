cbd1feb1bf68f5a420c8cf08ee44f4091be488ad
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-09-18 22:36:23 +0500

    Remove Vercel-specific sources

--- src/bot.mjs
+++ src/bot.mjs
@@ -5,9 +5,6 @@ export const {
     // Telegram bot token from t.me/BotFather
     TELEGRAM_BOT_TOKEN: token,
 
-    // Secret token to validate incoming updates
-    TELEGRAM_SECRET_TOKEN: secretToken = String(token).split(":").pop()
-
 } = process.env;
 
 // Default grammY bot instance
